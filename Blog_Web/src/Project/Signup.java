b804e6ff60c904d6eb64ef2da69ec7ea39d1b9c4
Owais Ahmed <ahmedt2owais@gmail.com>
2023-12-24 17:22:19 +0500

    Login and Signup Webservice Added

---
+++ Blog_Web/src/Project/Signup.java
@@ -0,0 +1,22 @@
+package Project;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Signup {
+	public String get_info_for_signup(String name,String email,String password) {
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con =
+			DriverManager.getConnection("jdbc:mysql://localhost:3306/blog","root","");
+			Statement stmt=con.createStatement();
+			stmt.executeUpdate("insert into user_table(Username,Email,password) values('"+name+"','"+email+"','"+password+"')");
+
+			con.close();
+		} catch(Exception e){
+			System.out.println(e);
+		}
+		return "";
+	}
+}
