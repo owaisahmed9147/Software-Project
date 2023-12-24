1365b8141686e7371f5eae7c0c9868dd9fcca8cd
Sheza <sp22bsse0031@maju.edu.pk>
2023-12-24 17:28:27 +0500

    editor web service created

---
+++ Blog_Web/src/Project/Add_Editor.java
@@ -0,0 +1,31 @@
+package Project;
+import java.sql.*;
+
+public class Add_Editor {
+	
+	public String getInfoForAdding(String password , String type ) {
+		
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con =
+			DriverManager.getConnection("jdbc:mysql://localhost:3306/blog","root","");
+			Statement stmt=con.createStatement();
+			
+			 int x = stmt.executeUpdate("insert into editor_table(editor_password , editor_type) values('"+password+"' , '"+type+"')");
+
+			 if(x > 0) {
+				 System.out.println("Query executed successfully");
+			 }
+			 else {
+				 System.out.println("Query not executed");
+			 }
+			con.close();
+			} catch(Exception e)
+			{
+			System.out.println(e);
+			}
+		
+		return "";
+	}
+
+}
