bb19cbca3565403eb0a6504d1bb03dd9d6dc7fe0
Owais Ahmed <ahmedt2owais@gmail.com>
2023-12-27 19:31:36 +0500

    updated sql file

--- blog.sql
+++ blog.sql
@@ -1,37 +1,270 @@
-package Project;
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 22, 2023 at 07:06 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
-import java.sql.ResultSet;
-import java.sql.Statement;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-public class blog_update {
 
-    public String blog_updating(int ID, String newTitle, String newContent) {
-        try {
-        	Class.forName("com.mysql.jdbc.Driver");
-			Connection con =
-			DriverManager.getConnection("jdbc:mysql://localhost:3306/blog","root","");
-			Statement stmt=con.createStatement();
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-            ResultSet resultSet = stmt.executeQuery("SELECT COUNT(*) FROM blog_post WHERE post_id = '" + ID + "'");
-            resultSet.next(); 
-            int existingCount = resultSet.getInt(1);
+--
+-- Database: `blog`
+--
 
-            if (existingCount > 0) {
-                
-                stmt.executeUpdate("UPDATE blog_post SET title = '" + newTitle + "', content = '" + newContent + "' WHERE post_id = '" + ID + "'");
-                System.out.println("Blog post updated successfully.");
-            } else {
-                System.out.println("Blog post not found for ID: " + ID);
-            }
+-- --------------------------------------------------------
 
-            con.close();
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
-        }
+--
+-- Table structure for table `administrator`
+--
 
-        return "";
-    }
-}
+CREATE TABLE `administrator` (
+  `admin_id` int(11) NOT NULL,
+  `admin_password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `administrator`
+--
+
+INSERT INTO `administrator` (`admin_id`, `admin_password`) VALUES
+(1, 'admin123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `author_info`
+--
+
+CREATE TABLE `author_info` (
+  `author_id` int(11) NOT NULL,
+  `author_username` varchar(100) NOT NULL,
+  `author_password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `author_info`
+--
+
+INSERT INTO `author_info` (`author_id`, `author_username`, `author_password`) VALUES
+(1, 'author1', '1author123'),
+(2, 'author2', '2author123'),
+(3, 'author3', '3author123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blog_post`
+--
+
+CREATE TABLE `blog_post` (
+  `post_id` int(11) NOT NULL,
+  `author_id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `content` varchar(10000) NOT NULL,
+  `created_at` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `blog_post`
+--
+
+INSERT INTO `blog_post` (`post_id`, `author_id`, `title`, `content`, `created_at`) VALUES
+(1, 2, 'lorem', 'Mashable is a world-renowned, entertainment, and multi-platform media channel.\r\n\r\nPowered by its own proprietary technology, this tech blog is the go-to source for tech, digital culture, and entertainment content for its influential and dedicated global audience!\r\n\r\nIt is one of the most influential blogs on technology on the internet today!\r\n\r\nMashable provides information to those who wish to catch up on all most everything happening around the tech world including movies, travel, finance, and, of course, gadgets.', '2023-12-21 10:25:17'),
+(2, 1, 'lorem', 'Lorem ipsum dolor sit amet. Est dolorem doloremque est sunt nobis et distinctio earum qui vitae consequatur. Qui ipsam officiis non pariatur inventore sit aperiam repellat et odio dolorem. Et culpa magni et quod itaque eos dicta magnam qui exercitationem sint 33 consectetur voluptatem', '2022-07-10 04:10:15');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `comment_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `content` varchar(5000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`comment_id`, `user_id`, `post_id`, `content`) VALUES
+(1, 3, 1, 'very good, brilliant work'),
+(2, 1, 1, 'excellent work');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `editor_table`
+--
+
+CREATE TABLE `editor_table` (
+  `editor_id` int(11) NOT NULL,
+  `editor_password` varchar(100) NOT NULL,
+  `editor_type` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `editor_table`
+--
+
+INSERT INTO `editor_table` (`editor_id`, `editor_password`, `editor_type`) VALUES
+(1, 'editor101', 'primary'),
+(2, 'editor102', 'secondary'),
+(3, 'editor103', 'guest');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `moderation`
+--
+
+CREATE TABLE `moderation` (
+  `moderation_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `editor_id` int(11) NOT NULL,
+  `status` varchar(100) NOT NULL,
+  `comments` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `moderation`
+--
+
+INSERT INTO `moderation` (`moderation_id`, `post_id`, `editor_id`, `status`, `comments`) VALUES
+(1, 2, 2, 'pending', 'resolving some grammatical mistakes');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_table`
+--
+
+CREATE TABLE `user_table` (
+  `User_ID` int(11) NOT NULL,
+  `Username` varchar(100) NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_table`
+--
+
+INSERT INTO `user_table` (`User_ID`, `Username`, `Email`, `password`) VALUES
+(1, 'Sid_Owais', 'owais@gmail.com', 'owais123'),
+(2, 'Aysha', 'aysha@gmail.com', 'aysha123'),
+(3, 'Sheza', 'sheza@gmail.com', 'sheza123');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `administrator`
+--
+ALTER TABLE `administrator`
+  ADD PRIMARY KEY (`admin_id`);
+
+--
+-- Indexes for table `author_info`
+--
+ALTER TABLE `author_info`
+  ADD PRIMARY KEY (`author_id`);
+
+--
+-- Indexes for table `blog_post`
+--
+ALTER TABLE `blog_post`
+  ADD PRIMARY KEY (`post_id`),
+  ADD KEY `author_id` (`author_id`);
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`comment_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `post_id` (`post_id`);
+
+--
+-- Indexes for table `editor_table`
+--
+ALTER TABLE `editor_table`
+  ADD PRIMARY KEY (`editor_id`);
+
+--
+-- Indexes for table `moderation`
+--
+ALTER TABLE `moderation`
+  ADD PRIMARY KEY (`moderation_id`),
+  ADD KEY `post_id` (`post_id`),
+  ADD KEY `editor_id` (`editor_id`);
+
+--
+-- Indexes for table `user_table`
+--
+ALTER TABLE `user_table`
+  ADD PRIMARY KEY (`User_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `administrator`
+--
+ALTER TABLE `administrator`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `author_info`
+--
+ALTER TABLE `author_info`
+  MODIFY `author_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `blog_post`
+--
+ALTER TABLE `blog_post`
+  MODIFY `post_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `editor_table`
+--
+ALTER TABLE `editor_table`
+  MODIFY `editor_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `moderation`
+--
+ALTER TABLE `moderation`
+  MODIFY `moderation_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user_table`
+--
+ALTER TABLE `user_table`
+  MODIFY `User_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
